efd4a02fa7ea7ff1600dcbbca145a03d624f9471
Jeffrey Lovell <eyekno3w@hotmail.com>
2018-07-20 16:50:07 -0700

    Made AJAX call from form submission, currently does not show data

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';      // Grab our fetch weather function
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
   constructor(props){
     super(props);
 
     this.state = { term: ''};
 
     this.onInputChange = this.onInputChange.bind(this); //replace existing function with the same function with it bound to "this"
+    this.onFormSubmit = this.onFormSubmit.bind(this);
   }
 
   onInputChange(event){
@@ -18,7 +22,8 @@ export default class SearchBar extends Component {
     event.preventDefault();
 
     // We need to go and fetch our weather
-    
+    this.props.fetchWeather(this.state.term);   // fetch weather for given city
+    this.setState({term: ''});                  // clear input
   }
 
   render() {
@@ -37,3 +42,10 @@ export default class SearchBar extends Component {
     );
   }
 }
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+// Pass in null, as this container doesn't use state, just dispatch
+export default connect(null, mapDispatchToProps)(SearchBar);
