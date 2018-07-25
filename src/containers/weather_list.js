4449bec5d344d6fb9993e043c6e0f0a18a77d80a
Jeffrey Lovell <eyekno3w@hotmail.com>
2018-07-25 11:27:56 -0700

    Weather charts now displaying, refactored out charts as a seperate component.

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import Chart from '../components/chart';
+
 class WeatherList extends Component{
   renderWeather(cityData){                  // Render each city data as a new row
     console.log('City Data: ', cityData);
     const name = cityData.city.name;        // Store name
+
+    // Collect data
+    const temps = cityData.list.map(weather => weather.main.temp);   // Collect temperature
+    console.log('Temperature array: ', temps);
+    const press = cityData.list.map(weather => weather.main.pressure );
+    const humis = cityData.list.map(weather => weather.main.humidity );
+    // return (
+    //   <tr key={name}>
+    //     <td>{name}</td>
+    //     <td>
+    //       <Sparklines height={120} width={180} data={temps}>
+    //         <SparklinesLine color="red" />
+    //       </Sparklines>
+    //     </td>
+    //     <td>
+    //       <Sparklines height={120} width={180} data={press}>
+    //         <SparklinesLine color="blue" />
+    //       </Sparklines>
+    //     </td>
+    //     <td>
+    //       <Sparklines height={120} width={180} data={humis}>
+    //         <SparklinesLine color="green" />
+    //       </Sparklines>
+    //     </td>
+    //   </tr>
+    // );
     return (
       <tr key={name}>
         <td>{name}</td>
+        <td>
+          <Chart data={temps} color="red" />
+        </td>
+        <td>
+          <Chart data={press} color="blue" />
+        </td>
+        <td>
+          <Chart data={humis} color="green" />
+        </td>
       </tr>
     );
   }
