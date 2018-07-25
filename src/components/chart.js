b57924f619a216add451614b00cad6388cc3b107
Jeffrey Lovell <eyekno3w@hotmail.com>
2018-07-25 14:29:11 -0700

    Added Google Map support to show the cities the weather forecast is for, as well as some styling changes to make it nicely formed.

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+import _ from 'lodash';
+
+function average(data){
+  return _.round(_.sum(data)/data.length)
+}
 // this function will generate a chart for display
 export default (props) => {
   return (
     <div>
       <Sparklines height={120} width={180} data={props.data}>
         <SparklinesLine color={props.color} />
+        <SparklinesReferenceLine type="avg" />
       </Sparklines>
+      <div>Average: {average(props.data)} {props.units}</div>
     </div>
   );
 }
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -2,14 +2,27 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import Chart from '../components/chart';
+import GoogleMap from '../components/google_map';
 
 class WeatherList extends Component{
+  constructor(props){
+    super(props);
+
+    // this.convertKToF = this.convertKToF.bind(this);
+    this.renderWeather = this.renderWeather.bind(this);     // Needed to access K To F conversion
+  }
+  convertKToF(temp){
+    return 1.8 * (temp - 273) + 32;
+  }
   renderWeather(cityData){                  // Render each city data as a new row
     console.log('City Data: ', cityData);
     const name = cityData.city.name;        // Store name
+    // const lon = cityData.city.coord.lon;    // Longitude
+    // const lat = cityData.city.coord.lat;    // Latitude
+    const { lon, lat } = cityData.city.coord;  // Get the two points in the given order 
 
     // Collect data
-    const temps = cityData.list.map(weather => weather.main.temp);   // Collect temperature
+    const temps = cityData.list.map(weather => this.convertKToF(weather.main.temp));   // Collect temperature
     console.log('Temperature array: ', temps);
     const press = cityData.list.map(weather => weather.main.pressure );
     const humis = cityData.list.map(weather => weather.main.humidity );
@@ -35,15 +48,15 @@ class WeatherList extends Component{
     // );
     return (
       <tr key={name}>
-        <td>{name}</td>
+        <td><GoogleMap lat={lat} lon={lon} /><div>{name}</div></td>
         <td>
-          <Chart data={temps} color="red" />
+          <Chart data={temps} color="red" units="F" />
         </td>
         <td>
-          <Chart data={press} color="blue" />
+          <Chart data={press} color="blue" units="hPa" />
         </td>
         <td>
-          <Chart data={humis} color="green" />
+          <Chart data={humis} color="green" units="%" />
         </td>
       </tr>
     );
@@ -55,9 +68,9 @@ class WeatherList extends Component{
         <thead>
           <tr>
             <th>City</th>
-            <th>Temperature</th>
-            <th>Pressure</th>
-            <th>Humidity</th>
+            <th>Temperature(F)</th>
+            <th>Pressure(HPa)</th>
+            <th>Humidity(%)</th>
           </tr>
         </thead>
         <tbody>
